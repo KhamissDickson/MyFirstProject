360f89875d6055708ad3f09d5b05a970d44ba827
PROMISE DICKSON <BSC/ELE/08/24@COMLAB.COM>
2026-04-15 04:06:53 +0200

    New files

---
+++ IntroToPointers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main () {
+    // declare a pointer and initialize it
+    // so that it does not store a random address
+    int* pPointer = nullptr;
+
+    int integerVar = 5;
+
+    // assign a pointer to address of object
+    pPointer = &integerVar;
+
+    //output the value of integerVar
+    cout << "integerVar: " << integerVar << endl;
+
+    //output the address of integerVar
+     cout << "Address of integerVar: " << &integerVar << endl;
+
+     //output the address of integerVar
+     cout << "pPointer: " << pPointer<< endl;
+
+     //output the address of pPointer
+     cout << "Address of pPointer: " << &pPointer << endl;
+
+     return 0;
+}
